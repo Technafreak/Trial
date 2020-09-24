b33c24380f931484e8dc0b7ca25c060529f7e888
TechnaFreak <vineetaritz@gmail.com>
2020-09-24 17:02:52 +0530

    Add Box2

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1;
+var box1,box2;
 
 function setup(){
     var canvas = createCanvas(400,400);
@@ -11,10 +11,12 @@ function setup(){
     world = engine.world;
 
     box1 = new Box(200,100,50,50);
+    box2 = new Box(300,100,50,100);
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
     box1.display();
+    box2.display();
 }
